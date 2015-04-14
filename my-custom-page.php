cbdc78e292def563b1abb2fae999ac66de6884fd
Riaz Hasan <riaz981@gmail.com>
2015-04-14 15:39:47 +1000

    Deleted unnecessary echo codes which were used for testing

--- my-custom-page.php
+++ my-custom-page.php
@@ -7,15 +7,9 @@ Template Name: My Custom Page
 <?php get_header(); ?>
 
 <?php
-
     $url = $_REQUEST['url'];
-    echo $url;
     $query="Select * from wp_property where url='".$url."'";
-
     $results = array_pop($wpdb->get_results($query));
-
-
-
 ?>
 <div class="container" id="container">
 
